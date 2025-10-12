83828a5dd9700e94a2993abe9eb07ca66f49870a
santymax98 <santymax9807@gmail.com>
2025-10-12 14:03:51 -0500

    add aqua_test

--- test/Aqua.jl
+++
@@ -1,8 +0,0 @@
-@testset "Aqua.jl" begin
-    Aqua.test_all(
-        AdditionalDistributions;
-        persistent_tasks = VERSION != v"1.10.10", # workaround for Julia 1.10.10
-        ambiguities = false,
-    )
-end
-
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,23 @@
+using Test
 using TestItems
 using TestItemRunner
-# --- OPCIÓN A: correr TODO ---
+using Aqua
+using AdditionalDistributions
+
+# --- Pruebas Aqua.jl ---
+@testset "Aqua.jl" begin
+    Aqua.test_all(
+        AdditionalDistributions;
+        persistent_tasks = VERSION != v"1.10.10", # workaround Julia 1.10.10 bug
+        ambiguities = false,  # opcional: puedes poner true si estás depurando métodos
+    )
+end
+
+# --- Ejecución de TestItems ---
+
+# OPCIÓN A: correr TODO el paquete
 @run_package_tests
 
-# --- OPCIÓN B: solo las pruebas con tag :Borel ---
-#@run_package_tests (filter = ti -> :Alpha in ti.tags)
+# --- OPCIÓN B (descomenta para filtrar por tag específico) ---
+# @run_package_tests (filter = ti -> :Alpha in ti.tags)
+# @run_package_tests (filter = ti -> :Borel in ti.tags)
